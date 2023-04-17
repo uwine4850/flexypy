9c9c2a925123ff2cae366a31958524865607660c
uwine <uwine4850@gmail.com>
2023-04-17 11:35:08 +0300

    Fix the main.py file generation error

--- flexypy/generate_templates/templates/main.py
+++ flexypy/generate_templates/templates/main.py
@@ -1,6 +1,12 @@
-from flexypy.http.server import WsgiServer
+try:
+    from flexypy.http.server import WsgiServer
+except Exception as e:
+    pass
 
 
 def application(environ, start_response):
-    ret = WsgiServer(environ, start_response).start()
-    return ret
+    try:
+        ret = WsgiServer(environ, start_response).start()
+        return ret
+    except Exception as e:
+        pass
